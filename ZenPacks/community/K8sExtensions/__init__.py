61870dea9a0b1e07e406c54624822cbfc262aace
hemie143 <hemie143@gmail.com>
2021-08-10 15:24:32 +0200

    Filtering mechanism for licensed pods Metrics Total of pods & licensed pods

--- ZenPacks/community/K8sExtensions/__init__.py
+++ ZenPacks/community/K8sExtensions/__init__.py
@@ -12,12 +12,55 @@ from Products.ZenModel.DataPointGraphPoint import DataPointGraphPoint
 # from Products.Zuul.form import schema
 
 class ZenPack(schema.ZenPack):
-    # TODO: Enhance list of ds, dp and gd and how this is handled
+
+    def customize_template(self, template, data):
+        # Datasource
+        for ds_name, ds_data in data['datasources'].items():
+            if hasattr(template.datasources, ds_name):
+                template.manage_deleteRRDDataSources([ds_name])
+            ds = template.manage_addRRDDataSource(ds_name, 'PythonDataSource.Python')
+            if 'plugin_classname' in ds_data:
+                ds.plugin_classname = ds_data['plugin_classname']
+            # Datapoint
+            for dp_name in ds_data['datapoints']:
+                dp = ds.manage_addRRDDataPoint(dp_name)
+                dp.rrdmin = 0
+
+        # Graph Definition
+        for gd_name, gd_data in data['graphs'].items():
+            if hasattr(template.graphDefs, gd_name):
+                template.manage_deleteGraphDefinitions([gd_name])
+            graph = template.manage_addGraphDefinition(gd_name)
+            graph.units = 'pods'
+            graph.miny = 0
+            # Graph Datapoints
+            dpNames = []
+            for gdp_name, gdp_data in gd_data['graphpoints'].items():
+                gdp = graph.manage_addDataPointGraphPoints(dpNames=[gdp_data['dpName']], includeThresholds=False)
+                gdp = gdp[0]
+                gdp.legend = ' ' + gdp_name
+        return
 
     def namespace_custom(self, app):
-        ds_name = 'count'
-        dp_name = 'podscount'
-        gd_name = 'Pods Count'
+        data = {
+            'datasources': {
+                'count': {
+                    'datapoints': {
+                        'podscount',
+                    },
+                    'plugin_classname': 'ZenPacks.community.K8sExtensions.dsplugins.namespace.Namespace'
+                }
+            },
+            'graphs': {
+                'Pods Count': {
+                    'graphpoints': {
+                        'Pods count': {
+                            'dpName': 'count_podscount',
+                        },
+                    }
+                }
+            }
+        }
 
         log.info('Customizing K8sNamespace template')
         try:
@@ -26,30 +69,34 @@ class ZenPack(schema.ZenPack):
         except:
             log.error('Template K8sNamespace not found')
             return
-
-        if hasattr(template.datasources, ds_name):
-            template.manage_deleteRRDDataSources([ds_name])
-        # Datasource
-        ds = template.manage_addRRDDataSource(ds_name, 'PythonDataSource.Python')
-        ds.plugin_classname = 'ZenPacks.community.K8sExtensions.dsplugins.namespace.Namespace'
-        ds.component = '${here/id}'
-        # Datapoint
-        dp = ds.manage_addRRDDataPoint(dp_name)
-        dp.rrdmin = 0
-        # Graph Definition
-        if hasattr(template.graphDefs, gd_name):
-            template.manage_deleteGraphDefinitions([gd_name])
-        graph = template.manage_addGraphDefinition(gd_name)
-        graph.units = 'pods'
-        graph.miny = 0
-        # Graph Datapoint
-        graph.manage_addDataPointGraphPoints(dpNames=[dp.name()], includeThresholds=False)
+        self.customize_template(template, data)
         log.info('Completed customization K8sNamespace template')
+        return
 
     def cluster_custom(self, app):
-        ds_name = 'count'
-        dp_name = 'totalpodscount'
-        gd_name = 'Pods Total Count'
+        data = {
+            'datasources': {
+                'count': {
+                    'datapoints': {
+                        'totalpodscount',
+                        'licpodscount',
+                    },
+                    'plugin_classname': 'ZenPacks.community.K8sExtensions.dsplugins.namespace.Namespace'
+                }
+            },
+            'graphs': {
+                'Pods Total Count': {
+                    'graphpoints': {
+                        'Total count': {
+                            'dpName': 'count_totalpodscount',
+                        },
+                        'Total count for licenses': {
+                            'dpName': 'count_licpodscount',
+                        }
+                    }
+                }
+            }
+        }
 
         log.info('Customizing K8sCluster template')
         try:
@@ -58,24 +105,7 @@ class ZenPack(schema.ZenPack):
         except:
             log.error('Template K8sCluster not found')
             return
-
-        if hasattr(template.datasources, ds_name):
-            template.manage_deleteRRDDataSources([ds_name])
-        # Datasource
-        ds = template.manage_addRRDDataSource(ds_name, 'PythonDataSource.Python')
-        ds.plugin_classname = 'ZenPacks.community.K8sExtensions.dsplugins.namespace.Namespace'
-        # ds.component = '${here/id}'
-        # Datapoint
-        dp = ds.manage_addRRDDataPoint(dp_name)
-        dp.rrdmin = 0
-        # Graph Definition
-        if hasattr(template.graphDefs, gd_name):
-            template.manage_deleteGraphDefinitions([gd_name])
-        graph = template.manage_addGraphDefinition(gd_name)
-        graph.units = 'pods'
-        graph.miny = 0
-        # Graph Datapoint
-        graph.manage_addDataPointGraphPoints(dpNames=[dp.name()], includeThresholds=False)
+        self.customize_template(template, data)
         log.info('Completed customization K8sCluster template')
 
     def install(self, app):
@@ -89,6 +119,7 @@ class ZenPack(schema.ZenPack):
         ZenPackBase.upgrade(self, app)
 
     def remove(self, app, leaveObjects=False):
+        # TODO: Don't hardcode the data. This implies that the data should be moved up from functions to class level.
         log.info('Cleaning up K8sCluster template')
         try:
             template = app.zport.dmd.Devices.Kubernetes.rrdTemplates.K8sCluster
--- ZenPacks/community/K8sExtensions/dsplugins/namespace.py
+++ ZenPacks/community/K8sExtensions/dsplugins/namespace.py
@@ -1,6 +1,7 @@
 
 # stdlib Imports
 import logging
+import re
 
 # Zenoss imports
 from ZenPacks.zenoss.PythonCollector.datasources.PythonDataSource import PythonDataSourcePlugin
@@ -12,9 +13,14 @@ log = logging.getLogger('zen.K8sExtensions')
 
 
 class Namespace(PythonDataSourcePlugin):
+    proxy_attributes = (
+        'zK8sLicNamespaceFilter',
+        'zK8sLicPodFilter',
+    )
 
     @classmethod
     def config_key(cls, datasource, context):
+        # TODO: Do not run for K8sCluster (remove in yaml ?)
         log.debug('In config_key {} {} {}'.format(context.device().id,
                                                   datasource.getCycleTime(context),
                                                   'k8sExt'))
@@ -50,14 +56,28 @@ class Namespace(PythonDataSourcePlugin):
     def onSuccess(self, results, config):
         log.debug('Success - results is {}'.format(results))
         data = self.new_data()
+
+        ds0 = config.datasources[0]
+        licPodsFilter = ds0.zK8sLicPodFilter
+        pattern = re.compile("(" + ")|(".join(licPodsFilter) + ")")
+
         total_pods = 0
+        lic_pods = 0
         for ds in config.datasources:
             if not ds.component:
                 continue
-            num_pods = len(results[ds.component])
-            data['values'][ds.component]['podscount'] = num_pods
+            pods = results[ds.component]
+            num_pods = len(pods)
+            for pod in pods:
+                # Remove first 4 chars as the id is prefixed with "pod-"
+                r = re.match(pattern, pod[4:])
+                if r:
+                    lic_pods += 1
+            data['values'][ds.component]['count_podscount'] = num_pods
             total_pods += num_pods
-        data['values'][None]['totalpodscount'] = total_pods
+
+        data['values'][None]['count_totalpodscount'] = total_pods
+        data['values'][None]['count_licpodscount'] = lic_pods
         log.debug('onSuccess - data: {}'.format(data))
         return data
 
